e9c8c2a3ae7323c66a844fae78ae9a8db99169ff
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-12-08 10:37:24 -0500

    login e registro

---
+++ registro-02/js/login.js
@@ -0,0 +1,17 @@
+// usei uma função, que para amim e mais rapido ser executado
+function entrar() {
+    let chavedeEntrada = document.getElementById("user").value;
+    let senha = document.getElementById("senha").value;
+
+        if (chavedeEntrada && senha ) {
+            window.location.href = "#";
+            alert("Seje bem-vindo!!");
+        }
+        else if(chavedeEntrada == "" || senha == ""){
+            alert("Verifique os campos não estão vazios");
+        }
+        else {
+            // caso o cara erre a senha. como não ta conectado a um banco de dados so tem uma senha possivel para acessar  o site.
+            alert("Porfavor verifique se colocou algum no campos do login!!");
+        }
+}
