de6c326c40d007fa3318cc24be6caecbde0ef347
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2024-12-08 10:20:42 -0500

    terminei o login

---
+++ login-01/js/login.js
@@ -0,0 +1,17 @@
+let array = ["user", 1234 ];
+// usei uma função, que para amim e mais rapido ser executado
+function entrar() {
+    let chavedeEntrada = document.getElementById("user").value;
+    let senha = document.getElementById("senha").value;
+
+        if (chavedeEntrada == array[0]  && senha == array[1] ) {
+            window.location.href = "#";
+        }
+        else if(chavedeEntrada == "" || senha == ""){
+            alert("Verifique os campos não estão vazios");
+        }
+        else {
+            // caso o cara erre a senha. como não ta conectado a um banco de dados so tem uma senha possivel para acessar  o site.
+            alert("Porfavor verifique se colocou algum no campos do login!!");
+        }
+}
