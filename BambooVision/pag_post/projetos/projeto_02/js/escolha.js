8959662a8deb0c9eb4fc79d14a18a372498e6788
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-01-05 14:54:34 -0300

    j

--- BambooVision/pag_post/projetos/projeto_02/js/escolha.js
+++ BambooVision/pag_post/projetos/projeto_02/js/escolha.js
@@ -20,20 +20,18 @@ if (document.getElementById("btn_03")) {
 function verificarEscolha(escolha) {
     let random = Math.floor(Math.random() * 3)
     if (escolha === random) {
-        positivo =+ 1;
         // aqui vamos selecionar a img
         if(escolha == 0){
-            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/pedra.png"
+            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/pedra.png";
         }
         else if(escolha == 1){
-            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/papel.png"
+            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/papel.png";
         }
         else if(escolha == 2){
-            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/tesoura.png"
+            document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/tesoura.png";
         }
     }
     else {
-        
-        document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/logo.png"
+        document.getElementById("EscolhaRandom").src="assents/main/escolha_machine/logo.png";
     }
 }
