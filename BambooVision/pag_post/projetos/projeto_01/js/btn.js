678854f802b6ef2762f7e93494a8e6e6b21fea2b
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-01-08 01:32:09 -0300

    lista

---
+++ BambooVision/pag_post/projetos/projeto_01/js/btn.js
@@ -0,0 +1,3 @@
+let btn = document.getElementById("voltaPerfil").addEventListener("click", function(){
+        window.location.href = "../../pagina_PostUser/index.html";
+})
---
+++ BambooVision/pag_post/projetos/projeto_02/js/funçãodoSite.js
@@ -0,0 +1,25 @@
+let lista = [];
+
+function escolher(){
+    let numero = document.getElementById("number").value;
+    //colocar na lista
+    if (numero == 1){
+        qtd = prompt("Quantidade de nomes a ser guardado: ");
+        for(let i = 0; i < qtd; i++){
+            lista[i] = prompt("Me de os nomes dos alimentos: ");
+        }
+        alert("lista feita!");
+    }
+    //ve lista
+    if(numero == 2){
+        if(lista != ''){
+            document.getElementById("lista").style.display = "block";
+        for(let i = 0; i < lista.length ; i++){
+            document.getElementById("listaTotal").innerHTML += lista[i] + "<br>";
+        }
+        }
+        else{
+            alert("Nada na lista!!");
+        }
+    }
+}
